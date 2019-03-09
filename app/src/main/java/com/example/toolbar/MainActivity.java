b2e21bc7f992f2da0aeb641dbed616c4175ddb33
vivek <vivek.koyani@gmail.com>
2019-03-09 18:13:55 +0530

    Initial commit

---
+++ app/src/main/java/com/example/toolbar/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.toolbar;
+
+
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+   private Toolbar toolbar;
+
+
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle("Hello App");
+        toolbar.setSubtitle("toolbar app");
+
+    }
+
+
+}
